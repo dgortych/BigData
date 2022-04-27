4fc10277096308d7432ed5ca3c0dd2e94a4365b9
Gortacik <75565389+dgortych@users.noreply.github.com>
2022-04-27 20:55:16 +0200

    Add files via upload

---
+++ Lab5/Cw5.py
@@ -0,0 +1,50 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##Najważniejsze narzędzia analityczne
+# MAGIC 
+# MAGIC AZURE
+# MAGIC 
+# MAGIC 1. Databricks 
+# MAGIC    https://docs.microsoft.com/en-us/azure/databricks/
+# MAGIC    
+# MAGIC 2. Data Factory
+# MAGIC    https://docs.microsoft.com/en-ca/azure/data-factory/
+# MAGIC 
+# MAGIC 3. Synapse Analytics
+# MAGIC    https://docs.microsoft.com/en-ca/azure/synapse-analytics/
+# MAGIC 
+# MAGIC 4. Machine Learning
+# MAGIC    https://docs.microsoft.com/en-ca/azure/machine-learning/
+# MAGIC 
+# MAGIC 5. Data Share 
+# MAGIC    https://docs.microsoft.com/en-ca/azure/data-share/
+# MAGIC 
+# MAGIC 6. Stream Analytics
+# MAGIC    https://docs.microsoft.com/en-ca/azure/stream-analytics/
+# MAGIC 
+# MAGIC 7. Data Lake Storage
+# MAGIC    https://docs.microsoft.com/en-ca/azure/data-lake-store/
+# MAGIC 
+# MAGIC 
+# MAGIC AWS
+# MAGIC 
+# MAGIC 1. AWS Glue
+# MAGIC    https://docs.aws.amazon.com/glue
+# MAGIC 
+# MAGIC 2. Amazon EMR
+# MAGIC    https://docs.aws.amazon.com/emr/
+# MAGIC 
+# MAGIC 3. Amazon RedShift
+# MAGIC    https://docs.aws.amazon.com/redshift
+# MAGIC 
+# MAGIC 4. AWS Data Pipeline 
+# MAGIC    https://docs.aws.amazon.com/data-pipeline/
+# MAGIC 
+# MAGIC 5. Amazon Athena
+# MAGIC    https://docs.aws.amazon.com/athena/
+# MAGIC 
+# MAGIC 6. Amazon Kinesis
+# MAGIC    https://docs.aws.amazon.com/kinesis
+# MAGIC 
+# MAGIC 7. AWS SageMaker
+# MAGIC    https://aws.amazon.com/pm/sagemaker/
